9dde93c22bb5cd7f0201c96d5330fb1410e4e96c
ArkanantaFC <arkafausta@gmail.com>
2025-05-18 23:11:30 +0700

    TPMODUL5_ARKANANTA

---
+++ MODUL5_ARKANANTA/TPMODUL5_ARKANANTA/routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+// TODO: Import controller MahasiswaController
+
+
+// TODO: Buat route untuk menangani request
+/*    - Rute-rute terkait CRUD mahasiswa:
+ *        - GET     `/mahasiswa`      → `MahasiswaController method:index`
+ *        - POST    `/mahasiswa`      → `MahasiswaController method:store`
+ *        - GET     `/mahasiswa/{id}` → `MahasiswaController method:show`
+ *        - PUT     `/mahasiswa/{id}` → `MahasiswaController method:update`
+ *        - DELETE  `/mahasiswa/{id}` → `MahasiswaController method:destroy`
+ */
